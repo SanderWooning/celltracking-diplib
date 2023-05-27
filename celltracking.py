7078d7f5889969a2710f8f6f0fe949cef1d4427d
Sander Wooning <s.wooning@newcompliance.com>
2023-05-27 22:21:33 +0200

    Added cell trajectory calculation

--- celltracking.py
+++ celltracking.py
@@ -5,6 +5,8 @@ import cv2
 from copy import deepcopy
 import matplotlib.cm as cm
 from PIL import Image
+import random
+import math
 
 
 def segment_image(image):
@@ -29,8 +31,8 @@ def segment_image(image):
     inverted_watershed = dip.Invert(watershed)
 
     # Edge object removal & Small component removal
-    removed_edge = dip.EdgeObjectsRemove(inverted_watershed)
-    removed_small = dip.SmallObjectsRemove(removed_edge, 2)
+    # removed_edge = dip.EdgeObjectsRemove(inverted_watershed)
+    removed_small = dip.SmallObjectsRemove(inverted_watershed, 2)
 
     labeled = dip.Label(removed_small, 0)
     labeled_array = np.asarray(labeled)
@@ -78,6 +80,11 @@ def segment_image(image):
     # plt.show()
 
     return labeled_array
+
+
+def random_rgb():
+    rgb = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+    return rgb
 
 
 def calculate_roundness(labeled_image, label):
@@ -185,30 +192,82 @@ def measurements(original_image, labeled_image):
 
 
 def make_gif(filename, image_stack):
-
     imgs = [Image.fromarray(img) for img in image_stack]
     # duration is the number of milliseconds between frames; this is 40 frames per second
-    imgs[0].save(f"gifs/{filename}.gif", save_all=True, append_images=imgs[1:], duration=500, loop=0)
+    imgs[0].save(f"gifs/{filename}.gif", save_all=True, append_images=imgs[1:], duration=250, loop=5)
 
+
+def distance_calc(point1_x, point2_x, point1_y, point2_y):
+    return math.sqrt((point2_x - point1_x) ** 2 + (point2_y - point1_y) ** 2)
+
+
+def calc_average_cell_distances(distance_list):
+
+
+    # Initialize a list to store average values
+    averages = []
+
+    # Get the length of the sublists (assuming equal length)
+    sublist_length = len(distance_list[0])
+
+    # Initialize a list to store average values
+    averages = []
+
+    # Iterate over the entry indices
+    for i in range(sublist_length):
+        total = 0
+        count = 0
+
+        # Iterate over the sublists
+        for sublist in distance_list:
+            total += sublist[i]
+            count += 1
+
+        # Calculate average and append to averages list
+        average = total / count
+        averages.append(average)
+
+    # Print the result
+    return averages
 
 
 if __name__ == "__main__":
 
-
     save_labels = []
     gif_images = []
     segment_images = []
+    centroid_list = []
+    label_list = []
+    pixel_distance = []
+
+    visualization_method = "PNG"
+
+    color_list = [random_rgb() for x in range(150)]
+
+    if visualization_method == "PNG":
+        # Generate an image to visualize the chosen cells with labeled numbers
+        visualization_image = cv2.imread(f"images/control/MTLn3-ctrl0029.tif")
+        # visualization_image = cv2.imread(f"images/subject/MTLn3+EGF0029.tif")
 
     # Control group
     for number in range(30):
+
+        # # Controlgroup
         if number < 10:
             image = cv2.imread(f"images/control/MTLn3-ctrl000{number}.tif")
 
         else:
             image = cv2.imread(f"images/control/MTLn3-ctrl00{number}.tif")
 
-        segmented = segment_image(image)
+        # Subject group
 
+        # if number < 10:
+        #     image = cv2.imread(f"images/subject/MTLn3+EGF000{number}.tif")
+        #
+        # else:
+        #     image = cv2.imread(f"images/subject/MTLn3+EGF00{number}.tif")
+
+        segmented = segment_image(image)
 
         # Choosing random cells closest to the middle
         # Choose 15 cells closest to the middle of the image
@@ -218,10 +277,13 @@ if __name__ == "__main__":
 
             previous_labels = choose_closest_cells_to_middle(labeled_image=segmented,
                                                              central_points=prev_central_points,
-                                                             amount_of_cells=30)
+                                                             amount_of_cells=15)
 
             display_cells = previous_labels
-            display_points = prev_central_points
+
+            label_list.append(previous_labels)
+            centroid_list.append(prev_central_points)
+            print(prev_central_points)
 
         if number != 0:
 
@@ -233,8 +295,8 @@ if __name__ == "__main__":
 
             for count, label in enumerate(previous_labels):
                 closest_point_list = neighbor_cell(previous_central_points=prev_central_points,
-                                              current_central_points=cur_central_points,
-                                              label=label)
+                                                   current_central_points=cur_central_points,
+                                                   label=label)
 
                 # To not have double points, check the closest that is not already in the list.
                 for point in closest_point_list:
@@ -242,36 +304,113 @@ if __name__ == "__main__":
                         closest_point = point
                         break
 
-
-
                 new_labels.append(closest_point)
 
             prev_central_points, prev_roundness_values, prev_intensity_values, prev_perimeter_values, prev_area_values = cur_central_points, cur_roundness_values, cur_intensity_values, cur_perimeter_values, cur_area_values
             previous_labels = new_labels
+
             display_cells = new_labels
-            display_points = cur_central_points
+            label_list.append(new_labels)
+            centroid_list.append(prev_central_points)
 
-            # Generate an image to visualize the chosen cells with labeled numbers
-        visualization_image = image
+        # PNG Visualization
 
-        counter = 1
+        if visualization_method == "PNG":
 
-        for label in display_cells:
-            point = display_points[label]
-            cv2.circle(visualization_image, (int(point[0]), int(point[1])), 5, (0, 0, 255), -1)
-            cv2.putText(visualization_image, str(counter), (int(point[0]) - 10, int(point[1]) - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            counter += 1
+            counter = 1
 
-        # Display the visualization image
-        gif_images.append(visualization_image)
-        segment_images.append(segmented)
+            distance_frame_list = []
+
+            for count, label in enumerate(display_cells):
+                current_point = centroid_list[number][label]
+
+                try:
+                    cv2.circle(visualization_image, (int(current_point[0]), int(current_point[1])), 2,
+                               color_list[count], -1)
+
+                    if number != 0:
+                        prev_point = centroid_list[number - 1][label_list[number - 1][count]]
+
+                        cv2.line(visualization_image, (int(prev_point[0]), int(prev_point[1])),
+                                 (int(current_point[0]), int(current_point[1])), color_list[count], 1)
+
+                        distance_frame_list.append(
+                            distance_calc(point1_x=int(prev_point[0]), point2_x=int(current_point[0]),
+                                          point1_y=int(prev_point[1]), point2_y=int(current_point[1])))
+
+                    if number == 29:
+                        cv2.putText(visualization_image, str(counter),
+                                    (int(current_point[0]) - 10, int(current_point[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.5, (255, 255, 255), 2)
+
+                except:
+                    continue
+
+                counter += 1
+
+            pixel_distance.append(distance_frame_list)
+
+        if visualization_method == "GIF":
+            visualization_image = image
+
+            counter = 1
+
+            for count, label in enumerate(display_cells):
+                point = centroid_list[number][label]
+                cv2.circle(visualization_image, (int(point[0]), int(point[1])), 5, color_list[count], -1)
+                cv2.putText(visualization_image, str(counter), (int(point[0]) - 10, int(point[1]) - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                counter += 1
+
+            plt.imsave("starting_image.png", visualization_image)
+
+            # Display the visualization image
+            gif_images.append(visualization_image)
+            segment_images.append(segmented)
 
 
-        save_labels.append([display_cells, display_points])
+    # Calculate trajectory data
+    dig = calc_average_cell_distances(pixel_distance[1:])
+    #print("Distance", dig)
+
+    print(np.average(dig))
 
 
 
 
-    make_gif("labeled", gif_images)
-    make_gif("segmented", segment_images )
+
+    print("")
+
+
+
+    vel = [x / 120 for x in dig]
+    # print("Vecolity pixel/second", vel)
+
+    print(np.average(vel))
+
+
+
+
+
+    #for frame in pixel_distance[1:]:
+
+
+
+
+
+    if visualization_method == "PNG":
+        plt.imsave("controlPNG15.png", visualization_image)
+
+    if visualization_method == "GIF":
+        make_gif("labeledCONTROL15", gif_images)
+        make_gif("segmentedCONTROL15", segment_images)
+
+    # FOR GIF
+    #
+    #     # Display the visualization image
+    #     gif_images.append(visualization_image)
+    #     segment_images.append(segmented)
+    #
+    #
+    # make_gif("labeled", gif_images)
+    # make_gif("segmented", segment_images )
